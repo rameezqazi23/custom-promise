00fba4df39e01b8deebb6c6b7d9de2c00a6b294d
rameezqazi23 <rameez.qazi23@gmail.com>
2025-01-12 02:52:20 +0500

    initialize reppository - new project, Build Custom Promise

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+//Create custom promise
+type TUser = {
+  name: string;
+  age: string | number;
+};
+
+const user: TUser = {
+  name: "Andrew",
+  age: 34,
+};
+
+console.log("User Print: ", user);
